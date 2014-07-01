4de0cd2169eafa7af217a88c92b844b5f089b18f
emilyadavis303 <emilyadavis303@gmail.com>
2014-07-01 17:04:43 -0600

    Makes transaction repository test match updated transaction repository.

--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -3,7 +3,7 @@ require_relative '../lib/transaction_repository'
 
 class TransactionRepositoryTest < Minitest::Test
   def setup
-    @repo = TransactionRepository.new("./test/fixtures/transactions_sample.csv")
+    @repo = TransactionRepository.new
   end
 
   def test_it_exists
@@ -13,4 +13,15 @@ class TransactionRepositoryTest < Minitest::Test
   def test_a_repository_loads_transactions
     assert @repo.count >= 10
   end
+
+  def test_it_can_find_a_transaction_by_cc_number
+    result = @repo.find_by_credit_card_number('4203696133194408')
+    assert result.credit_card_number, '4203696133194408'
+    assert result.id, '5536'
+  end
+
+  # def test_it_can_find_all_by_result
+  #   result = @repo.find_by_status('success')
+  # end
+
 end
