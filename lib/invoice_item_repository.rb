6a45b1f75c4915ce474cbb6d27908bc7a50d6c23
Ephemere <objectivparadigm@gmail.com>
2014-07-01 16:59:42 -0600

    adding invoice item searches

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -2,18 +2,27 @@ require 'csv'
 require_relative '../lib/invoice_item'
 
 class InvoiceItemRepository
-  attr_reader :invoice_items
+  attr_reader :invoice_items, :invoice_id
 
   def initialize(filename='./data/invoice_items.csv')
-    invoice_item_data = CSV.open(filename, headers: true, header_converters: :symbol)
-
-    @invoice_items = invoice_item_data.collect do |invoice_item|
-      invoice_item
-    end
-
+    item_data = CSV.open(filename, headers: true, header_converters: :symbol)
+    @invoice_items = item_data.collect { |item| item }
   end
 
   def count
     @invoice_items.count
   end
+
+  def find_by_item_id(id)
+    @invoice_items.detect { |item| id.to_s == item[0] }
+  end
+
+  def find_all_by_quanity(quantity)
+    @invoice_items.select { |item| quantity.to_s == item[3] }
+  end
+
+  def find_by_id(invoice_id)
+    @invoice_items.detect { |item| invoice_id.to_s == item[2] }
+  end
+
 end
