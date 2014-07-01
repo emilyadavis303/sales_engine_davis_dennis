4500ab4565a669772b9e799c1af22051fc554e31
emilyadavis303 <emilyadavis303@gmail.com>
2014-07-01 17:16:40 -0600

    Now I believe the test.

--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -15,9 +15,9 @@ class TransactionRepositoryTest < Minitest::Test
   end
 
   def test_it_can_find_a_transaction_by_cc_number
-    result = @repo.find_by_credit_card_number('4203696133194408')
-    assert result.credit_card_number, '4203696133194408'
-    assert result.id, '5536'
+    result = @repo.find_by_credit_card_number('4140149827486249')
+    assert_equal '4140149827486249', result.credit_card_number
+    assert_equal '9', result.id
   end
 
   # def test_it_can_find_all_by_result
