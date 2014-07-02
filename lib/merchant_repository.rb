0a5342ad85e934292ab089b1beb87e48a10eb00a
emilyadavis303 <emilyadavis303@gmail.com>
2014-07-01 21:18:11 -0600

    Adds and tests find_by methods for Merchant Repository.

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -15,6 +15,18 @@ class MerchantRepository
     @merchants = repository.map {|row| Merchant.new(row)}
   end
 
+  def find_by_name(name)
+    @merchants.find {
+      |merchant| merchant.name == name
+    }
+  end
+
+  def find_all_by_name(name)
+    @merchants.find_all {
+      |merchant| merchant.name == name
+    }
+  end
+  
   def count
     @merchants.count
   end
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -14,8 +14,14 @@ class MerchantRepositoryTest < Minitest::Test
     assert @repo.count >= 10
   end
 
-  # def test_if_merchants_were_really_created
-  #   assert_equal 'Willms and Sons', @repo.name.include?
-  # end
+  def test_can_find_a_single_merchant_by_name
+    result = @repo.find_by_name('Osinski, Pollich and Koelpin')
+    assert_equal 'Osinski, Pollich and Koelpin', result.name
+    assert_equal '8', result.id
+  end
 
+  def test_can_find_all_merchants_by_name
+    result = @repo.find_all_by_name('Williamson Group')
+    assert_equal 2, result.count
+  end
 end
