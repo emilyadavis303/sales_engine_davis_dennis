1f016fb687085ef93d03e032e6c0c10a3fa1efd5
emilyadavis303 <emilyadavis303@gmail.com>
2014-07-02 15:13:53 -0600

    adds find by methods

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -9,6 +9,24 @@ class InvoiceRepository
     @invoices = Parser.new.parse(filename, Invoice)
   end
 
+  def find_by_status(status)
+    @invoices.find {
+      |invoice| invoice.status == status
+    }
+  end
+
+  def find_all_by_status(status)
+    @invoices.find_all {
+      |invoice| invoice.status == status
+    }
+  end
+
+  def find_by_id(id)
+    @invoices.find {
+      |invoice| invoice.id.to_s == id.to_s
+    }
+  end
+
   def count
     @invoices.count
   end
